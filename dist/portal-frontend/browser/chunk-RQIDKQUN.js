9962194bfa9849e43d911af7e99d497e41f786a9
TigselemaAlex <kaiser.alex1994@gmail.com>
2024-06-03 10:51:13 -0500

    Logs for routes

--- dist/portal-frontend/browser/chunk-RQIDKQUN.js
+++
@@ -1,1 +0,0 @@
-var t={API_URL:"http://www.api.portaldelavinia.com/portal/api/v1"};export{t as a};
---
+++ dist/portal-frontend/browser/chunk-TDXTCVPJ.js
@@ -0,0 +1,1 @@
+var t={API_URL:"https://www.api.portaldelavinia.com/portal/api/v1"};export{t as a};
